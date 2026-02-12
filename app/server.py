ca8977d8d8a5a5a77fbd57b612091895496c8e68
Vasileios Markopoulos <vmarkofrl@gmail.com>
2026-02-12 13:59:50 +0200

    Prometheus and Grafana Added

--- app/server.py
+++ app/server.py
@@ -1,20 +1,27 @@
-from flask import Flask
-import os           # <--- NEW: Needed for self-destruction
 import socket
+import time
+import os
+from flask import Flask
+from prometheus_flask_exporter import PrometheusMetrics # <--- NEW
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    container_id = socket.gethostname()
-    return f"🚀 VERSION 2.0! Server: {container_id}\n", 200
+# --- 📊 METRICS SETUP ---
+# This automatically exposes a /metrics endpoint for Prometheus
+metrics = PrometheusMetrics(app)
+metrics.info('app_info', 'Application info', version='1.0.3')
+# ------------------------
 
-# --- THE CHAOS ENDPOINT ---
+@app.route('/')
+def home():
+    container_id = socket.gethostname()
+    return f"✅ [Corrected] Response from: {container_id}\n"
+
 @app.route('/crash')
 def crash():
-    # Force the process to exit immediately with an error code
-    # This simulates a fatal application crash.
-    os._exit(1)  
+    # The Chaos Monkey trigger
+    print("💥 CRITICAL ERROR: Committing suicide...")
+    os._exit(1)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
